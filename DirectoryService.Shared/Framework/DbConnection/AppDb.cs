a62f3ef45f7e8714410ebb35a050bc81b85eb68f
brinersam <vladbudyachenko@mail.ru>
2025-09-19 03:04:41 +0300

    Обертка над IDbConnection, IDbTransaction с nullobject версией для простоты исопльзования OpenTransactionIfNotOngoing

---
+++ DirectoryService.Shared/Framework/DbConnection/AppDb.cs
@@ -0,0 +1,63 @@
+﻿using System.Data;
+using IsolationLevel = System.Data.IsolationLevel;
+
+namespace DirectoryService.Shared.Framework;
+public class AppDb
+{
+    public IDbConnection Connection { get; init; }
+
+    private AppDbTransaction _transactionWrapper;
+
+    public IDbTransaction? Transaction { get; private set; }
+
+    public IsolationLevel? TransactionIsolationLevel => Transaction?.IsolationLevel;
+
+    public bool HasOngoingTransaction => Transaction is not null;
+
+    public bool HasIsolationLevel => TransactionIsolationLevel is not null;
+
+    public AppDb(IDbConnection connection)
+    {
+        Connection = connection;
+    }
+
+    public IDbTransaction BeginTransaction()
+    {
+        if (Transaction is null)
+        {
+            _transactionWrapper = new AppDbTransaction(this);
+            Transaction = _transactionWrapper.Transaction;
+        }
+
+        return Transaction;
+    }
+
+    public IDbTransaction BeginTransaction(IsolationLevel il)
+    {
+        if (Transaction is null)
+        {
+            _transactionWrapper = new AppDbTransaction(this, il);
+            Transaction = _transactionWrapper.Transaction;
+        }
+
+        return Transaction;
+    }
+
+    public void OnTransactionDisposed()
+    {
+        Transaction = null;
+    }
+
+    public IDbTransaction OpenTransactionIfNotOngoing()
+    {
+        if (Transaction is null)
+        {
+            BeginTransaction();
+            return _transactionWrapper;
+        }
+        else
+        {
+            return new NullObjectDbTransaction(Transaction);
+        }
+    }
+}
